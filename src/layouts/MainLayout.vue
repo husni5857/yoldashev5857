7a35106c40a9676672c6cc793b4889b494bab529
husni5857 <hyoldashev63@gmail.com>
2022-04-20 17:12:45 +0500

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="hhh lpR fff">
+    <q-header style="height:100px;" bordered class="bg-transparent text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <div class="q-mt-lg fit row no-wrap justify-center items-center content-start">
+            <div>
+              <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/white.png">
+            </div>
+     
+          <div class="" v-for="select in selects" :key="select">
+<div class="fit row no-wrap justify-center items-center content-start">
+    <q-btn-dropdown color="transparent" unelevated  :label='select.label' >
+      <q-list>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>{{select.name}}</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>{{select.fname}}</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>{{select.namef}}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+</div>
+  </div>
+
+
+          </div>
+             </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      onItemClick () {
+        // console.log('Clicked on an Item')
+      },
+      selects: [
+        
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'HOME' },
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'PAGES' },
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'SHOWCASES' },
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'BLOG' },
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'SHOP' },
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'PRICING' },
+        {name: 'MOUNTAIN BIKING', fname: 'SNOWBOARDING', namef: 'TRAIL RUNNING', label:'CONTACT US' }
+
+      ]
+    }
+  }
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <q-img style="  position: absolute; top: 0%;"  src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-2.png" >
+          <div class="absolute-full  column flex flex-center bg-transparent">
+            <span class="text-h6">LIVE BRIGHT RIDE HARD</span>
+            <span class="text-h3 q-mt-md" >Live Bright <span class="bg-dark text-yellow">Ride Hard</span> </span>
+          <q-btn class="bg-yellow text-dark q-mt-xl">FINT OUT HOW</q-btn>
+          </div>
+      </q-img>
+      <div style="height:600px;"></div>
+    <div class="row">
+      <div style="width:50%;" class=" q-ml-xl">
+        <span class="text-grey">ABOUT US</span><br><br>
+      <span class="text-h5">When Snowboard Is <span class="bg-dark text-yellow">Your Life</span></span><br><br>
+      <span style="font-size:17px;" class="text-grey">Incredible scenery, an adrenaline rush or simply being out in the mountains are a few of the reasons people enjoy snowboarding. It's a great activity to get involved in and is more affordable than you might think if you plan ahead and are clever about a few bits and bobs.</span>
+<div class=" line-of-icons">
+  <div class=" fit row wrap justify-around items-center content-start line-icons-wrap">
+     <div class="icons-item ">
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group.png">
+      <span>Equipment</span>
+     </div>
+     <div class="icons-item">
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if_cap_hat_ski_winter_2664884.png">
+      <span>Clothes</span>
+     </div>
+     <div class="icons-item">
+      <img src="https://xtrm.foxthemes.me/wp-content/uploads/2018/09/if_smartwatch_watch_fitness_pulse_sport_2664858-2.png">
+      <span>Accessories</span>
+     </div>
+  </div>
+</div>
+    </div>
+      <q-video class="q-ml-lg" style="width:40%; height:300px;"
+      src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0"/>
+    </div>
+    
+    <q-img src="http://xtrm.foxthemes.me/wp-content/uploads/2018/09/Group-26.png?id=3390)">
+      <div class="bg-transparent fit row q-mt-xl ">
+        <div style="width:50%; height:100%;"></div>
+        <div style="width:50%; height:100%;">
+          <span>EVENTS</span><br><br>
+        <span class="text-h5">The Legendary Tyler Durden's <span class="bg-dark text-yellow">Workshop</span></span><br><br>
+        <span>It doesn’t matter if you are a stay at home mom, or a corporate professional. You need to keep your appearance at it’s best all the time, and that can be a hard job. But what if you don’t always have the time to go to a Snowboard for a professional facial? I’ve got just the solution for you.</span><br><br><br>
+        <q-btn class="bg-dark q-pl-xl q-pr-xl q-pt-sm q-pb-sm"  label="BUY NOW"/>
+        <q-btn class="q-ml-xl q-pl-xl q-pr-xl q-pt-sm q-pb-sm learn" label="LEARN MORE" />
+        </div>
+        
+      </div>
+    </q-img>
+     
+  </div>
+</template>
+<script>
+
+export default ({
+
+})
+</script>
+<style scoped>
+.learn{
+  border: 2px solid white;
+}
+</style>
